2ccb07934c11c2db4d7647ec83570072520ccb2e
Mehul <mehulr2801@gmail.com>
2026-08-16 21:11:12 +0000

    chore: add free-tier deployment configs

--- dist/hedera/client.js
+++ dist/hedera/client.js
@@ -16,6 +16,8 @@ const TRANSIENT_PATTERNS = [
     'ECONNRESET',
     'ETIMEDOUT',
 ];
+// Pinata API endpoint for IPFS pinning
+const PINATA_API_URL = 'https://api.pinata.cloud/pinning/pinJSONToIPFS';
 /**
  * Thin wrapper around the Hedera Consensus/Token Service SDK that adds:
  *   - exponential-backoff retry for transient HCS publish errors,
