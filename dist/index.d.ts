1e5846a0b2ffafe2eb5b16ba18956f5d86a671eb
Mehul <mehulr2801@gmail.com>
2026-08-17 01:56:56 +0000

    chore: add free-tier deployment configs

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,4 +3,5 @@ export { AgentRegistry } from './agent-registry.js';
 export { Marketplace } from './marketplace.js';
 export { Governance } from './governance.js';
 export type * from './types.js';
+export type { NexusChainMarketplace, NexusChainMarketplaceInterface, AgentDeactivatedEvent, AgentListedEvent, AgentPurchasedEvent, AgentRatedEvent, AgentRegisteredEvent, AgentUpgradedEvent, ProposalCreatedEvent, ProposalExecutedEvent, VoteCastEvent, } from '../typechain-types';
 //# sourceMappingURL=index.d.ts.map
